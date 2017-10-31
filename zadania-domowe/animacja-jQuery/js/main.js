4662734a6d65cdf514f3f944a07285f478fd2eef
Mariusz Orbik <mariusz.orbik@gmail.com>
2017-10-31 21:14:00 +0100

    poprawki interpunkcji

--- zadania-domowe/animacja-jQuery/js/main.js
+++ zadania-domowe/animacja-jQuery/js/main.js
@@ -20,7 +20,8 @@ $('button').click(function () {
     $('#kwadrat').animate({
         width: '100px',
         height: '100px',
-        left: '100px',
+        left: '200px',
+        marginTop: '240px',
     }, 3000, drugaAnimacja);
     
 });
