5cb7f1ccf7ebb7bfe4bb63f8eb6b2994732f6647
Mariusz Orbik <mariusz.orbik@gmail.com>
2017-10-31 16:10:17 +0100

    zrobiona animacja kwadratu

---
+++ zadania-domowe/animacja-jQuery/js/main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function trzeciaAnimacja() {
+    $('h2').css('opacity', '1');
+};
+
+function drugaAnimacja() {
+    $('#kwadratWewnetrzny').animate({
+        opacity: 1,
+    }, 5000, trzeciaAnimacja)
+};
+
+
+$('button').click(function () {
+    //    console.log('kliknięto guzik');
+    $('#kwadrat').animate({
+        width: '100px',
+        height: '100px',
+        left: '100px',
+    }, 3000, drugaAnimacja)
+});
