07a9910d00f92b43fa89b547e8c3dfe56fba97ab
Mariusz Orbik <mariusz.orbik@gmail.com>
2017-11-18 16:57:33 +0100

    nagłówek i formularz

--- zadania-domowe/gulp-sass/gulpfile.js
+++ zadania-domowe/gulp-sass/gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-    return gulp.src('app/scss/style.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+gulp.task('sass', function() {
+    'use strict';
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('app/scss/style.scss', ['sass']);
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
 });
+
+
